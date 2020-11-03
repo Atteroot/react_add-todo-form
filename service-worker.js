33b340f1f8d259df0de8d56fbde999077f2bb1f1
Atteroot <yuora556@gmail.com>
2020-11-03 21:31:49 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.8f567177e417e077463092be3c969c5e.js"
 );
 
 self.addEventListener('message', (event) => {
